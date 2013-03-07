767ed1510c70ff80d2c1d1bca7b4a51786c5d814
Sébastien Testeau <stesteau@gmail.com>
2013-03-07 16:49:43 -0500

    first commit

--- js/backbone/lib/backbone-filetree.js
+++
@@ -1,9 +0,0 @@
-// A file tree view javascript class
-//     Accepts formatted JSON:
-//     Root Node ->
-//     {
-//         path: "path",
-//         name: "name",
-//         nodes: [ array of nodes ]
-//     }
-
